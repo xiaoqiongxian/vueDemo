07b06e154d69912df09bbdb25a869b2cad09d2c8
xiaoqiongxian <2918417295@qq.com>
2018-04-04 15:02:47 +0800

    Add files via upload

---
+++ src/components/pageOne.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>{{ $t("message.hello") }}</p>
+        <p v-text="$t('message.hello')"></p>
+        <el-button type="primary" v-on:click="testAlert">test</el-button>
+        <el-button type="primary">主要按钮</el-button>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                page: 'vue-router'
+            }
+        },
+        methods:{
+           testAlert:function(){
+                alert(this.$t('message.hello'));
+           } 
+        }
+    }
+</script>
---
+++ src/components/pageTwo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="">
+        <div>这是第二个页面</div>
+    </div>
+</template>
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                page: 'pageTwo'
+            }
+        }
+    }
+</script>
